be0d7ad9c4965d51860cd8c5ddb7845c70889e6f
ngthuc <lenguyenthuc.edu@gmail.com>
2020-07-21 21:39:22 +0700

    Update pattern

--- phone_regex_pattern.ts
+++ phone_regex_pattern.ts
@@ -1,7 +1,7 @@
 /**
  * @author: ngthuc
  * @created_date: Jun 22nd, 2020
- * @lasted_update: Jul 20th, 2020
+ * @lasted_update: Jul 21th, 2020
  * @homepage: https://ngthuc.com/
  * @repository: https://github.com/ngthucdotcom/vietnam-phone-regex-pattern
  * @license: MIT
@@ -19,18 +19,18 @@
  * => Telephone in Vietnam
  * Source: https://vi.wikipedia.org/wiki/Mã_điện_thoại_Việt_Nam
  */
-export const phone_regex_pattern: string = '((032|033|034|035|036|037|038|039|052|055|056|058|059|070|076|077|078|079)+([0-9]{7})\\b)|(08+([1-9]{8})\\b)|(09+([0-9]{8})\\b)|(02+([0-9]{9})\\b)';
+export const phone_regex_pattern: string = '((03+([2-9]{1})|05+([2]{1}|[5-6]{1}|[8-9]{1})|07+([0]{1}|[6-9]{1}))+([0-9]{7})\\b)|(08+([1-9]{8})\\b)|(09+([0-9]{8})\\b)|(02+([0-9]{9})\\b)';
 
-// /**
-//  * Usage: to check email
-//  */
-// export const email_regex_pattern: string = '(([^<>()\\[\\]\\.,;:\\s@\\"]+(\\.[^<>()\\[\\]\\.,;:\\s@\\"]+)*)|(\\".+\\"))@(([^<>()[\\]\\.,;:\\s@\\"]+\\.)+[^<>()[\\]\\.,;:\\s@\\"]{2,})';
-
-// /**
-//  * Usage: to check a password from 8 characters more which contain at least:
-//  * - one lowercase letter,
-//  * - one uppercase letter,
-//  * - one numeric digit,
-//  * - and one special character
-//  */
-// export const password_regex_pattern: string = '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\\s).{8,}$';
+/**
+ * Email pattern
+ * Usage: to check email
+ * export const email_regex_pattern: string = '(([^<>()\\[\\]\\.,;:\\s@\\"]+(\\.[^<>()\\[\\]\\.,;:\\s@\\"]+)*)|(\\".+\\"))@(([^<>()[\\]\\.,;:\\s@\\"]+\\.)+[^<>()[\\]\\.,;:\\s@\\"]{2,})';
+ *
+ * Password pattern
+ * Usage: to check a password from 8 characters more which contain at least:
+ * - one lowercase letter,
+ * - one uppercase letter,
+ * - one numeric digit,
+ * - and one special character
+ * export const password_regex_pattern: string = '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\\s).{8,}$';
+*/

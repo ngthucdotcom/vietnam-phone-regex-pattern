4783f32d01666de5295893f468037eab3c95ee00
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2020-08-26 09:06:07 +0700

    Update phone_regex_pattern.ts

--- phone_regex_pattern.ts
+++ phone_regex_pattern.ts
@@ -1,22 +1,24 @@
 /**
  * @author: ngthuc
  * @created_date: Jun 22nd, 2020
- * @lasted_update: Aug 10th, 2020
+ * @lasted_update: Aug 26th, 2020
  * @homepage: https://ngthuc.com/
  * @repository: https://github.com/ngthucdotcom/vietnam-phone-regex-pattern
  * @license: MIT
  */
 
-/** Phone Regular Expression Pattern
- * Detail: ((03+([2-9]{1})|05+([2]{1}|[5-6]{1}|[8-9]{1})|07+([0]{1}|[6-9]{1})|08+([1-9]{1})|09+([0-9]{1}))+([0-9]{7})\b)|(02+([0-9]{9})\b)
+/**
+ * Phone Regular Expression Pattern
  * Description:
  * - Group 1: (03+([2-9]{1})|05+([2]{1}|[5-6]{1}|[8-9]{1})|07+([0]{1}|[6-9]{1})|08+([1-9]{1})|09+([0-9]{1}))+([0-9]{7})\b)
  * => Mobile phone of Viettel/Vietnamobile/Reddi/Gmobile/MobiFone/Vinaphone/Itelecom/Gmobile in Vietnam
  * - Group 2: (02+([0-9]{9})\b)
  * => Telephone in Vietnam
+ * - Group 3: ((1800|1900)+([0-9]{4,6})\b)
+ * => PBX phone in Vietnam
  * Source: https://vi.wikipedia.org/wiki/Mã_điện_thoại_Việt_Nam
  */
-export const phone_regex_pattern: string = '((03+([2-9]{1})|05+([2]{1}|[5-6]{1}|[8-9]{1})|07+([0]{1}|[6-9]{1})|08+([1-9]{1})|09+([0-9]{1}))+([0-9]{7})\b)|(02+([0-9]{9})\b)';
+export const phoneRegexPattern: string = '((03+([2-9]{1})|05+([2]{1}|[5-6]{1}|[8-9]{1})|07+([0]{1}|[6-9]{1})|08+([1-9]{1})|09+([0-9]{1}))+([0-9]{7})\\b)|(02+([0-9]{9})\\b)|((1800|1900)+([0-9]{4,6})\\b)';
 
 /**
  * Email pattern
